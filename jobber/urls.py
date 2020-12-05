a98cdf3b916305739787579e04108d5b04e4b808
Andrew <andrew@Andrew-2.local>
2020-12-05 20:18:22 +0800

    basic login, register, layout.html

--- jobber/urls.py
+++ jobber/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.listing, name="listing")
+    path("listing", views.listing, name="listing")
     ]
--- jobber/views.py
+++ jobber/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
 
 # Create your views here.
+def listing(request):
+    return HttpResponse('Welcome to Jobber!')
